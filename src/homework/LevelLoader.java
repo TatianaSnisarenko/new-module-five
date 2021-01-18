9a02ca80baea493ffdf1e9ba9d96a231faff558c
Tatiana Snisarenko <tatiana.snisarenko.p@gmail.com>
2021-01-18 22:00:20 +0200

    add task 16-29

---
+++ src/homework/LevelLoader.java
@@ -0,0 +1,23 @@
+package homework;
+
+class LevelLoader {
+    private static LevelLoader instance = new LevelLoader();
+
+    public static LevelLoader getInstance(){
+        return instance;
+    }
+
+    private LevelLoader() {
+    }
+
+    public String load(String levelName){
+        return "Loading level " + levelName + " ...";
+    }
+}
+
+class LevelLoaderTest {
+    public static void main(String[] args) {
+        //Loading level Startlevel ...
+        System.out.println(LevelLoader.getInstance().load("StartLevel"));
+    }
+}
